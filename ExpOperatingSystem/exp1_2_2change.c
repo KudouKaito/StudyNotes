e0ec7c179e952c077cd953fb0b77d5ae86e02ae6
KudouKaito <lhy634@outlook.com>
2021-05-03 15:38:05 +0800

    操作系统第一次实验代码

--- ExpOperatingSystem/exp1_2_2change.c
+++ ExpOperatingSystem/exp1_2_2change.c
@@ -6,14 +6,19 @@ int main()
     uint_8t integer;
     uint_8t n_five;
     uint_8t n_two;
+    uint_8t i = 0;
     printf("请输入面额为十元倍数的整钱(<=100):");
     scanf("%hhu", &integer);
     if (integer%10 != 0  || integer/10 <= 0) {
         printf("ERROR: 面额必须为十的倍数.");
         return -1;
     }
-    for (n_five = 1; 5*n_five <= integer-3; n_five++) 
-        for (n_two = 1; 2*n_two+5*n_five <= integer-1; n_two++)
+    for (n_five = 1; 5*n_five <= integer-3; n_five++) {
+        for (n_two = 1; 2*n_two+5*n_five <= integer-1; n_two++) {
+            i++;
             printf("5元:%d 2元:%d 1元:%d\n", n_five, n_two, integer-5*n_five-2*n_two);
+        }
+    }
+    printf("共%hhu种", i);
     return 0;
 }
